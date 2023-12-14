8ab73907d777e7819baf223b860ca299817549a2
Gabriel Moreyra <gabrielmoreyra97@gmail.com>
2023-12-13 23:56:56 -0300

    modificada pagina principal

--- batalla-naval/src/App.jsx
+++ batalla-naval/src/App.jsx
@@ -13,32 +13,10 @@ function App() {
     <Router>
       <Routes>
         {/* <Route path="*" element={<Error404 />} /> */}
-        <Route path="/" element={<MainMenu />} />
-        <Route path="/game" element={<Game />} />
+        {/* <Route path="/" element={<MainMenu />} /> */}
+        <Route path="/" element={<Game />} />
       </Routes>
     </Router>
-    // <>
-    //   <div>
-    //     <a href="https://vitejs.dev" target="_blank">
-    //       <img src={viteLogo} className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://react.dev" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </>
   )
 }
 
