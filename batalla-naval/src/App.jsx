09d93e5087d0ea7eefc6d92331143df5a7b58e2a
Gabriel Moreyra <gabrielmoreyra97@gmail.com>
2023-12-14 00:39:04 -0300

    Quitado import que rompia el proyecto

--- batalla-naval/src/App.jsx
+++ batalla-naval/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MainMenu from './components/MainMenu'
 import Game from './components/Game'
 import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 
